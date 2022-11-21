655416a6a7ccc2a95a60a8e18486e0e0abbfadb6
somtochukwu nwosu <somynwosu@gmail.com>
2022-11-21 16:19:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+  }
+  
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Please wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App container flex flex-col justify-center items-center gap-5 p-100  ">
+			<h1 className="text-5xl font-bold text-center my-4"> All Users </h1> {
+				items.map((item) => (
+          <div className="text-1xl box-border w-100 flex flex-row gap-20 justify-center text-center items-center" key={item.id}>
+            <div className="p-5 rounded-md shadow-2xl">
+              <p className="card-text f-3 border-solid">Username: { item.username }</p> 
+				      <p className="border-solid">User email: { item.email } </p>
+            </div>
+					</div>
+				))
+			}
+		</div>
+	);
+}
 }
 
 export default App;
+
